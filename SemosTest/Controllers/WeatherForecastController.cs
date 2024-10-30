ce6b648f1c8effbf3fa6a9df313003cce5a28146
Your Name <you@example.com>
2024-10-30 11:33:22 +0100

    Add project files.

---
+++ SemosTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SemosTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("Get")]
+        public IActionResult Get()
+        {
+            return Ok(Summaries);
+        }
+    }
+}
