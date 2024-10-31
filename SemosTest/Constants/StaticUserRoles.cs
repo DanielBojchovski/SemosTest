680b808f0156bbe4283d20e4b3d08d39d74e5dbd
Your Name <you@example.com>
2024-10-31 18:02:12 +0100

    Seed roles

---
+++ SemosTest/Constants/StaticUserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace SemosTest.Models
+{
+    public static class StaticUserRoles
+    {
+        public const string USER = "USER";
+        public const string ADMIN = "ADMIN";
+    }
+}
---
+++ SemosTest/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SemosTest.Models;
+
+namespace SemosTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AuthController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpPost("SeedRoles")]
+        public async Task<IActionResult> SeedRoles()
+        {
+            bool userRolesExists = await _roleManager.RoleExistsAsync(StaticUserRoles.USER);
+            bool adminRolesExists = await _roleManager.RoleExistsAsync(StaticUserRoles.ADMIN);
+
+            if (userRolesExists && adminRolesExists) 
+            { 
+                return Ok("Seeding is already done"); 
+            }
+
+            await _roleManager.CreateAsync(new IdentityRole(StaticUserRoles.USER));
+            await _roleManager.CreateAsync(new IdentityRole(StaticUserRoles.ADMIN));
+
+            return Ok("Role seeding completed");
+        }
+    }
+}
